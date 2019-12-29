9d17188b5e59bb0bdde8f30123d6bb888b38cc5c
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-29 12:05:44 +0530

    Initial commit

---
+++ Operators/String Arithmetic Operator.cpp
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+    	
+    	String X, Y, Z, result;
+    	
+    	X = "one ";
+    	Y = "+ one ";
+    	Z = "= eleven";
+    	
+    	result = X+Y+Z;
+    	System.out.println(result);
+    }
+}
