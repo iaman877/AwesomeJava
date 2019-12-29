eb5ee3c18043a80c5b1cf6f2d04e472e63376ff5
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-29 11:45:41 +0530

    Initial commit

---
+++ Operators/Assignment Operator.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+    	int n1, n2;
+    	n1 = 10;             // Assigning 10 to n1
+    	System.out.println(n1);
+    	n2 = n1;    	// Assigning value of variable n2 to n1
+    	System.out.println(n2);
+    }
+}
