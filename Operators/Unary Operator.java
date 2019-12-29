b08ace13ea53f34ef8959cf65800fa2da2b39a44
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-29 12:14:22 +0530

    initial commit

---
+++ Operators/Unary Operator.java
@@ -0,0 +1,16 @@
+class Main {
+
+     public static void main(String[] args){
+          int result = +1; 
+          System.out.println(result);
+          result--;  
+          System.out.println(result);
+          result++; 
+          System.out.println(result);
+          result = -result; 
+          System.out.println(result);
+          boolean success = false;
+          System.out.println(success);       // false
+          System.out.println(!success);      // true
+     }
+}
