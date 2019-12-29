eb8308ccba8524a6968041fb25f8bcf80456a7d9
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-29 12:20:48 +0530

    initial commit

---
+++ Operators/Equality and Relational Operators.java
@@ -0,0 +1,21 @@
+class Main {
+
+    public static void main(String[] args){
+        int v1 = 1;
+        int v2 = 2;
+        if(v1 == v2)
+            System.out.println("v1 == v2");
+        if(v1 != v2)
+            System.out.println("v1 != v2");
+        if(v1 > v2)
+            System.out.println("v1 > v2");
+        if(v1 < v2)
+            System.out.println("v1 < v2");
+        if(v1 <= v2)
+            System.out.println("v1 <= v2");
+         if((v1 == 1) && (v2 == 2))
+            System.out.println("v1 is 1 AND v2 is 2");
+        if((v1 == 1) || (v2 == 1))
+            System.out.println("v1 is 1 OR v2 is 1");
+    }
+}
