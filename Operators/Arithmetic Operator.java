4c07afa8a14443f2a742e1d32dfed2aca3f117f0
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-29 12:00:33 +0530

    Initial commit

---
+++ Operators/Arithmetic Operator.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+    	double n1 = 19.5, n2 = 2.5, result;
+    	result = n1 + n2;      	//  addition operator
+    	System.out.println("number1 + number2 = " + result);
+    	result = n1 - n2;   	// subtraction operator
+    	System.out.println("number1 - number2 = " + result);
+    	result = n1 * n2;      	// multiplication operator
+    	System.out.println("number1 * number2 = " + result);
+    	result = n1 / n2;        // division operator
+    	System.out.println("number1 / number2 = " + result);
+    	result = n1 % n2;     	// remainder operator
+    	System.out.println("number1 % number2 = " + result);
+    }
+}
