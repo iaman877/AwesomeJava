e2055ced91f1e598478a74865a1df5eeb97b6d8c
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:51:51 +0530

    Initial Commit

---
+++ loop/while/Infinite while loop.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String args[]){
+         int X=10;
+         while(X>1)
+         {
+             System.out.println(X);
+              X++;
+         }
+    }
+}
