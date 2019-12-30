f452c0642cc92c4ff35da98deaf779710b9b52a6
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:31:54 +0530

    Initial commit

---
+++ loop/for-loop/Simple for loop2.java
@@ -0,0 +1,13 @@
+// Program to find the sum of natural numbers from 1 to 1000.
+class Main {
+   public static void main(String[] args) {
+      
+      int x = 0;
+	   
+      for (int i = 1; i <= 1000; ++i) {
+         x += i;    
+      }
+	   
+      System.out.println("Sum = " + x);
+   }
+}
