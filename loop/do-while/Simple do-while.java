63be9b044f6a0244aaf0c9e4c11a05e358218170
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:57:27 +0530

    Initial Commit

---
+++ loop/do-while/Simple do-while.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String args[]){
+         int X=10;
+         do{
+              System.out.println(X);
+              X--;
+         }while(X>=1);
+    }
+}
