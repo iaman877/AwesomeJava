d1794981499fe75842666cf752356ee19c03f1b1
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:59:25 +0530

    Initial Commit

---
+++ loop/do-while/array using do-while loop.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String args[]){
+         int arr[]={2,11,45,9};
+         //i starts with 0 as array index starts with 0
+         int X=0;
+         do{
+              System.out.println(arr[X]);
+              X++;
+         }while(X<4);
+    }
+}
