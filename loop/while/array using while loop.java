2fca36a7cdf54d9ea3bb2d087ee6366e4a78fb3b
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:54:03 +0530

    Initial Commit

---
+++ loop/while/array using while loop.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String args[]){
+         int arr[]={2,11,45,9};
+         //i starts with 0 as array index starts with 0 too
+         int X=0;
+         while(X<4){
+              System.out.println(arr[X]);
+              X++;
+         }
+    }
+}
