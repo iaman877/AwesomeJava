b35eaa37f0592222a9c8dcafced541f08cf17f7d
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:28:34 +0530

    Initial commit

---
+++ loop/for-loop/Simple for loop.java
@@ -0,0 +1,9 @@
+// Program to print a sentence 10 times
+class Main {
+   public static void main(String[] args) {
+      
+      for (int i = 1; i <= 10; ++i) {
+         System.out.println(+i);
+      }
+   }
+}
