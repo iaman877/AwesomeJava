7e8532e85c813445e9a77afdd1d41271f6068f8c
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 16:46:05 +0530

    Initial commit

---
+++ Operators/ternary operator.java
@@ -0,0 +1,10 @@
+public class Main {
+   public static void main(String args[]) {
+    int n1, n2;
+    n1 = 25;
+	n2 = (n1 == 10) ? 100: 200;
+	System.out.println( "num2: "+n2);
+	n2 = (n1 == 25) ? 100: 200;
+	System.out.println( "num2: "+n2);
+   }
+}
