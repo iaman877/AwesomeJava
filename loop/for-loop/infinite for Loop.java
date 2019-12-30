f7b8cded7408cdd38bad424d9de19b1a3377b3be
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2019-12-30 17:34:32 +0530

    Initial commit

---
+++ loop/for-loop/infinite for Loop.java
@@ -0,0 +1,9 @@
+class Main{
+   public static void main(String[] args) {
+      
+      int x = 0;
+      for (int i = 1; i <= 10; --i) {
+         System.out.println("Hello world");
+      }
+   }
+}
