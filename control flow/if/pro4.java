a3e4f2aa7389632b92e6d1b2d0ef954da440bf96
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-15 22:03:24 +0530

    initial commit

---
+++ control flow/if/pro4.java
@@ -0,0 +1,7 @@
+public class Main{    
+public static void main(String[] args) {    
+    int number=21;    
+    String output=(number%2==0)?"it is even number":" it is odd number";    
+    System.out.println(output);  
+ }    
+}    
