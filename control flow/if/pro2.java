2543794f502a5ad07877c464575165b1408e7650
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-15 21:57:23 +0530

    initial commit

---
+++ control flow/if/pro2.java
@@ -0,0 +1,10 @@
+public class Main {  
+public static void main(String[] args) {  
+    int number=5;  
+    if(number%2==0){  
+        System.out.println("given number is even number");  
+    }else{  
+        System.out.println("given number is odd number");  
+    }  
+ }  
+}  
