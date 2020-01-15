cb73c38ce586464d97f2f0dea702f3e336e1e9e1
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-15 22:09:42 +0530

    Initial commit

---
+++ control flow/switch/pro1.java
@@ -0,0 +1,14 @@
+public class Main {  
+public static void main(String[] args) {  
+    int number=10;  
+    switch(number){  
+    case 10: System.out.println("10");  
+    break;  
+    case 20: System.out.println("20");  
+    break;  
+    case 30: System.out.println("30");  
+    break;  
+    default:System.out.println("Nothing");   //Default case statement  
+    }  
+ }  
+}  
