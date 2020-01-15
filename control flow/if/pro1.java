9fd6bbbacbac19c63ddf2debd669e3cb0767c5e4
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-15 21:54:22 +0530

    intial commit

---
+++ control flow/if/pro1.java
@@ -0,0 +1,8 @@
+public class Main {  
+public static void main(String[] args) {  
+    int age=19;  
+    if(age>18){  
+        System.out.print("your Age is greater than 18");  
+    }  
+  }  
+}  
