07265a772497bc58e95c66463862062774298235
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-16 13:12:27 +0530

    Initial Commit

---
+++ Lab Experiment/lab1/Gretest_among.java
@@ -0,0 +1,11 @@
+class Sum{
+	public static void main(String args[]){
+                System.out.println("My name is Aman Bhardwaj & roll  is 09 .");
+		int result=0;
+		for(int i=41;i<250;i++){
+			if(i%5==0)
+				result+=i;
+		}
+		System.out.println("Sum of the number is : "+result);
+	}
+}
