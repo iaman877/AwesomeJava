116b027b74457ab458495619d10e78c408592f5a
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-16 13:15:51 +0530

    initial commit

---
+++ Lab Experiment/lab1/Gretest_among3.java
@@ -0,0 +1,12 @@
+ class Greater {
+    public static void main(String[] args) {
+        System.out.println("My name is Aman Bhardwaj & roll  is 09 .");
+        double n1 = -.5, n2 = .9, n3 = 5.5;
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
