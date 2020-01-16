4c3c7178075e515377459994ed1c24b178ac81ce
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-16 13:11:29 +0530

    initial commit

---
+++ Lab Experiment/lab1/Fibbonacci.java
@@ -0,0 +1,14 @@
+ class Fibb {
+    public static void main(String[] args) {
+        System.out.println("My name is Aman Bhardwaj & roll  is 09 .");
+        int n = 10, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
