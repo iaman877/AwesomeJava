16295b97bee6a1aff0d99488caac5cc2b70ea5c6
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-22 22:59:45 +0530

    Initial Commit

---
+++ INPUT FROM USER/using CMD3.java
@@ -0,0 +1,8 @@
+class AddCline 
+ { 
+public static void main(String args[]) 
+  { 
+ double a =Double.parseDouble(args[0]);  double b =Double.parseDouble(args[1]);  double c = a + b; 
+ System.out.println("Sum of a and b is :"+ c); 
+     } 
+ } 
