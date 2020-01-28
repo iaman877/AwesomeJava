3c749f1b9f92d437bb09648384aae23ae7189bb8
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-01-29 00:52:00 +0530

    Initial Commit

---
+++ Array/Pro1.java
@@ -0,0 +1,19 @@
+class Main{
+public static void main(String args[]){
+int a[]=new int[5];
+a[0]=10;
+a[1]=20;
+a[2]=70;
+a[3]=40;
+a[4]=50;
+for(int i=0;i<a.length;i++)//length is the property of array
+System.out.println(a[i]);
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+cars[0] = "Opel";
+System.out.println(cars[0]);
+System.out.println(cars.length);
+
+int intArray[];            //declaring array
+intArray = new int[20];    // allocating memory to array
+ }
+}
