8757453ff1b9d9c33bfb0d764fa83506d178f880
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-12 22:02:45 +0530

    Abstract class

---
+++ Keyword/Abstract/Program1.java
@@ -0,0 +1,80 @@
+// Like C++, in Java, an instance of an abstract class cannot be created, we can have references of abstract class type though.
+
+abstract class A1 
+{ 
+	abstract void print(); 
+} 
+class Derived extends A1 
+{ 
+	void print()
+	    {
+	        System.out.println("Derived print() called");
+	    } 
+} 
+class Main
+{ 
+	public static void main(String args[]) { 
+		A1 b = new Derived(); 
+		b.print(); 
+	} 
+} 
+//---------------------------------------------------------------------------------------------------------------------
+
+// An abstract class with constructor 
+
+abstract class A1
+{ 
+	A1() 
+	{
+	    System.out.println("Base Constructor Called"); 
+	} 
+     	abstract void fun(); 
+} 
+class Derived extends A1
+{ 
+	Derived()
+	  {
+	      System.out.println("Derived Constructor Called");
+	  } 
+	void fun() 
+	    {
+	        System.out.println("Derived fun() called");
+	    } 
+} 
+class Main { 
+	public static void main(String args[]) { 
+	Derived d = new Derived(); 
+	d.fun();
+	} 
+} 
+//----------------------------------------------------------------------------------------------------------------------
+
+// An abstract class without any abstract method 
+
+abstract class A1
+{ 
+	void fun() 
+	   { 
+	       System.out.println("Base fun() called");
+	   } 
+} 
+
+class Derived extends A1
+{    
+        Derived()
+          {
+              System.out.println("Derived Constructor Called"); 
+          } 
+       void fun() 
+            {
+                System.out.println("Derived fun() called");
+            } 
+} 
+
+class Main { 
+	public static void main(String args[])
+	{ 
+		Derived d = new Derived(); 
+		d.fun(); 
+	} 
+} 
