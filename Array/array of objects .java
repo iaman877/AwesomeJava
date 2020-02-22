d6fed9a32b2af9537f547751030b488b64515d3b
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-22 23:28:10 +0530

    Initial commit

---
+++ Array/array of objects .java
@@ -0,0 +1,26 @@
+// Java program to illustrate creating an array of objects 
+class Student 
+{ 
+	public int roll_no; 
+	public String name; 
+	Student(int roll_no, String name) 
+	{ 
+		this.roll_no = roll_no; 
+		this.name = name; 
+	} 
+} 
+public class Main
+{ 
+	public static void main (String[] args) 
+	{ 
+		Student[] arr; 
+		arr = new Student[5]; 
+		arr[0] = new Student(1,"aman"); 
+		arr[1] = new Student(2,"vaibhav"); 
+		arr[2] = new Student(3,"shikar"); 
+		arr[3] = new Student(4,"Anjani"); 
+		arr[4] = new Student(5,"Prateek"); 
+		for (int i = 0; i < arr.length; i++) 
+			System.out.println(arr[i].roll_no +" "+ arr[i].name); 
+	} 
+} 
