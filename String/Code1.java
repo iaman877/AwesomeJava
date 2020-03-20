8f01dc6ffcf67d39a0e80db718c151aa6057c6a6
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-20 23:00:13 +0530

    introduction to string

---
+++ String/Code1.java
@@ -0,0 +1,14 @@
+// Java code to illustrate String 
+
+import java.io.*; 
+import java.lang.*; 
+
+class Main { 
+	public static void main(String[] args) 
+	{ 
+		String s = "Aman"; 		// Declare String without using new operator 
+		System.out.println("String s = " + s);  // Prints the String. 
+		String s1 = new String("Aman");    	// Declare String using new operator
+		System.out.println("String s1 = " + s1);  // Prints the String. 
+	} 
+} 
