3ab23ce26bccb5bf0f4341efb6f2e8933b305ea7
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-21 12:31:34 +0530

    substring function

---
+++ String/Code4.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Main {
+   public static void main(String[] args) {
+      String name = "Amanbhardwaj";
+      System.out.println(name.substring(0,4));  // Aman
+       System.out.println(name.substring(4));   // bhardwaj
+
+   }
+}
