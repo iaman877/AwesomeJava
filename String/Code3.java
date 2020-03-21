7c867a95fb2df0d9ee667cc26de15a3b5c991263
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-21 12:06:37 +0530

    charAt function

---
+++ String/Code3.java
@@ -0,0 +1,11 @@
+// charAt() function
+public class Main
+ {  
+   public static void main(String args[]){  
+   String name="Amanbhardwaj";  
+   char ch1=name.charAt(3);//returns the char value at the 3th index
+   char ch2=name.charAt(4);//returns the char value at the 4th index
+   char ch3=name.charAt(5);//returns the char value at the 5th index
+   System.out.println(ch1+""+ch2+""+ch3);  
+ }
+} 
