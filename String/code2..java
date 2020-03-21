1f5e619c38698fb2d93ae2cb9319edd873ecb6e4
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-21 12:03:54 +0530

    length() function

---
+++ String/code2..java
@@ -0,0 +1,11 @@
+//  length() function
+
+public class Main {
+    public static void main(String[] args) {
+        //declare the String as an object S1 S2
+        String S1 = "Hello I am from India ";
+        String S2 = "Amazing";
+        System.out.println("Length of a String is: " + S1.length());
+        System.out.println("Length of a String is: " + S2.length());
+    }
+}
