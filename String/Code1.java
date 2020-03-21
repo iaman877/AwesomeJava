1f38b7df4f6e6e7c6568d3a07a967c7d3ff58f75
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-21 11:34:48 +0530

    Update Code1.java

--- String/Code1.java
+++ String/Code1.java
@@ -10,5 +10,6 @@ class Main {
 		System.out.println("String s = " + s);  // Prints the String. 
 		String s1 = new String("Aman");    	// Declare String using new operator
 		System.out.println("String s1 = " + s1);  // Prints the String. 
+		System.out.println(s == s1);   // false
 	} 
 } 
