1ab56f802e78dacf35fb8d86e8587fec63a797b4
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-21 14:40:25 +0530

    contains() function

---
+++ String/Code5.java
@@ -0,0 +1,16 @@
+// contains() function 
+public class Main {
+    public static void main(String args[]) {
+        String str1 = "Aman Anshika Happy";
+        // In If-else statements you can use the contains() method
+
+        if (str1.contains("Happy"))
+        {
+            System.out.println("The Keyword :example: is found in given string");
+        }
+        else 
+        {
+            System.out.println("The Keyword :example: is not found in the string");
+        }
+    }
+}
