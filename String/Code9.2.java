afc1d7778f9def9efe6e12464bfa0f6ddad12555
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-22 13:35:20 +0530

    toLowerCase & toUpperCase function

---
+++ String/Code9.2.java
@@ -0,0 +1,12 @@
+// toLowerCase & toUpperCase function is return type string
+import java.io.*; 
+import java.lang.*; 
+
+class Main { 
+	public static void main(String[] args) 
+	{ 
+		String cars = "Hyundai,Maruti,Swift,Wagnor,Ferrari";
+		System.out.println(cars.toLowerCase());
+		System.out.println(cars.toUpperCase());
+	} 
+}  
