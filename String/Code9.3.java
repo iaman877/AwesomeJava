94af97d40d5c0e582c2bc3f77959ebfb32f844ce
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-22 13:44:56 +0530

    trim function

---
+++ String/Code9.3.java
@@ -0,0 +1,11 @@
+// trim function is return type string
+import java.io.*; 
+import java.lang.*; 
+
+class Main { 
+	public static void main(String[] args) 
+	{ 
+		String name = "   Aman     ";
+		System.out.println(name.trim());
+	} 
+}  
