07b5ce0f45e9bb161cd479be83cfb65d47fda2f9
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-23 20:45:13 +0530

    Create Code3.java

---
+++ Multithreading/by  Runnable/Code3.java
@@ -0,0 +1,31 @@
+class Mythread1 implements Runnable
+{
+  public void run() 
+  {
+     for(int i=0;i<10;i++) 
+       {
+          System.out.println("Running Thread1:"+i);
+       }
+  }
+}
+class Mythread2 extends Thread
+ {
+    public void run()
+     {
+       for(int i=10;i<20;i++)
+         {
+            System.out.println("Running Thread2:"+i); 
+         }
+     }
+ }
+class Main 
+ {
+   public static void main(String arg[])
+    {
+      Mythread1 r1=new Mythread1();
+      Thread t1=new Thread(r1,"thread1");
+      t1.start();
+      Mythread2 r2=new Mythread2();
+      r2.start();
+    }
+}
