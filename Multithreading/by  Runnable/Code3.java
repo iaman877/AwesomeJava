ea05e5b2b2600bb42c872b02871e0ddcae6715ad
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-26 22:51:53 +0530

    we will get mixed output

--- Multithreading/by  Runnable/Code3.java
+++ Multithreading/by  Runnable/Code3.java
@@ -1,12 +1,12 @@
 class Mythread1 implements Runnable
 {
-  public void run() 
-  {
+   public void run() 
+   {
      for(int i=0;i<10;i++) 
        {
           System.out.println("Running Thread1:"+i);
        }
-  }
+   }
 }
 class Mythread2 extends Thread
  {
