4c4e5c884d8c7977294dae93fdbd82dcd0e37be0
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-29 20:59:08 +0530

    Create Autoboxing.java

---
+++ Wrapper class/Autoboxing.java
@@ -0,0 +1,12 @@
+public class PriToWrapper
+{
+   public static void main(String args[]){
+   //Converting int into Integer
+   int a=20;
+   Integer i=Integer.valueOf(a);//converting int into Integer
+   Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally
+   System.out.println(a);
+   System.out.println(j);
+   System.out.println(i);
+ }
+}
