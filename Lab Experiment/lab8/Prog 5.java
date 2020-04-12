6e6178f77001f19a6f9c8774a4188f12480aac0c
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-04-12 12:38:44 +0530

    Update Prog 5.java

--- Lab Experiment/lab8/Prog 5.java
+++ Lab Experiment/lab8/Prog 5.java
@@ -1,3 +1,6 @@
+// Write a Java code that converts int to Integer, converts Integer to String, converts String to int, converts int to String, converts String to Integer converts Integer to int.
+
+
 class Wrapper1
 { 
 public static void main(String args[]) 
@@ -22,4 +25,4 @@ public static void main(String args[])
      String strValue1 = String.valueOf(intParam);
      System.out.println("int equvialent to String = " +strValue1);
  } 
-} 
+}
