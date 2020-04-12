85ed8f294f0c61a158fdc4933c4819d30449aab9
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-04-12 12:28:13 +0530

    Update Prog 3.java

--- Lab Experiment/lab8/Prog 3.java
+++ Lab Experiment/lab8/Prog 3.java
@@ -1,14 +1,14 @@
-import java.util.*;
-class Aman3
+//Write a program in Java to read a statement from console, convert it into upper case and again print on console. (Don’t use inbuilt function) 
+
+
+ import java.io.*; 
+class  Aman2
 {
-public static void main(String args[])
-{
-StringBuffer str= new StringBuffer();
-Scanner sc= new Scanner(System.in);
-System.out.println("Enter a string buffer");
-str.append(sc.nextLine());
-String str1 = str.toString();
-String str2=str1.toUpperCase();
-System.out.println("\nThe converted string buffer input  in capital letters string is:"+str2);
-}
+   public static void main(String a[]) throws IOException
+    {
+      DataInputStream in=new DataInputStream(System.in); 
+      System.out.println("Enter the string ");
+      String s1=in.readLine();
+      System.out.println("upper case of String  "+s1.toUpperCase());
+  }
 }
