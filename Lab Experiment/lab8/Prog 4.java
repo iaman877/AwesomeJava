562c2ff28bd1aa139d40e83a893b1a2eb1dd0091
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-04-12 12:37:40 +0530

    Update Prog 4.java

--- Lab Experiment/lab8/Prog 4.java
+++ Lab Experiment/lab8/Prog 4.java
@@ -1,3 +1,6 @@
+// Write a program in Java to create a String object. Initialize this object with your name. Find the length of your name using the appropriate String method. Find whether the character ‘a’ is in your name or not; if yes find the number of times ‘a’ appears in your name. Print locations of occurrences of ‘a’ .Try the same for different String objects
+
+
 class check
 {
 String name;
