7c455c8785fbfe58d6f25ed88f358a5b154a55bf
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-04-12 12:08:06 +0530

    Create Prog 5.java

---
+++ Lab Experiment/lab8/Prog 5.java
@@ -0,0 +1,25 @@
+class Wrapper1
+{ 
+public static void main(String args[]) 
+{ 
+    int i = 10;
+    Integer intObj = new Integer(i);
+    System.out.println("int equvialent to  Integer = " +intObj);
+	int a = 1234; 
+	String str1 = Integer.toString(a); 
+	System.out.println("Integer equvialent to  String  = " + str1); 
+	 String s = "100";
+    try
+    {
+      int j = Integer.parseInt(s.trim());
+      System.out.println("String equvialent to int = " + j);
+    }
+    catch (NumberFormatException nfe)
+    {
+      System.out.println("NumberFormatException: " + nfe.getMessage());
+    }
+     int intParam = 1001;
+     String strValue1 = String.valueOf(intParam);
+     System.out.println("int equvialent to String = " +strValue1);
+ } 
+} 
